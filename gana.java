411788d6db0b36cad3f4a9523addd7e036c7a10c
Ganagood <123107731+Ganagood@users.noreply.github.com>
2023-01-19 23:39:21 +0530

    Update gana.java

--- gana.java
+++ gana.java
@@ -1,1 +1,19 @@
-system.out.println("gana");
+package simple_java;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+
+public class java5 {
+
+	public static void main(String[] args) {
+
+
+		System.out.println("i am ganesh");
+
+		
+	}
+
+}
