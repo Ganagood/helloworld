ae76daaba59e90f58b94377479de56f28be2524d
Ganagood <123107731+Ganagood@users.noreply.github.com>
2023-01-19 23:11:49 +0530

    Add files via upload

---
+++ java5.java
@@ -0,0 +1,41 @@
+package simple_java;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+
+public class java5 {
+
+	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		int a=23;
+//		int b=35;
+//		int c;
+//		c=a+b;
+//		System.out.println("the output value :"+c);
+//		
+//		String con="USA/Canada";
+//	    String x="//*[@id=\\\"country_id\\\"]/ul//li[contains(@name,"+con+")]\"";
+	    //WebElement we=driver.findElement(By.xpath("//*[@id=\"country_id\"]/ul//li[contains(@name,con)]"));
+	   // System.out.println(x);
+		 //String t="22_2_2000";
+//		 int p=22-3-2000;
+//		 String r=Integer.toString(p);
+//		 System.out.println(r);
+//		String g="female";
+//		String f="female";D
+//		String m="male";
+//		if(g==f) {
+//			System.out.println("w");
+//			
+//		}
+//		else {
+//			System.out.println("b");
+		System.out.println("i am ganesh");
+//		}	
+		
+	}
+
+}
